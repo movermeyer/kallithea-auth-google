c0fb5c940fcadd51fe0c5e95e68154af4cd7ae41
Anatoly Bubenkov <bubenkoff@gmail.com>
2015-04-21 23:43:41 +0200

    correctly handle identities

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     license="MIT license",
     author_email="developers@paylogic.com",
     url="https://github.com/paylogic/kallithea-google-auth",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(include="kallithea_auth_google*"),
     install_requires=install_requires,
     entry_points="""
